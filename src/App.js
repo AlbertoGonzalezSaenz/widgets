0ac68bf1101ee790cfe96c2c35efa5ccf94ab80e
AlbertoGonzalezSaenz <alberto.gonzalez@kinandcarta.com>
2022-01-17 10:35:09 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Accordion from './components/accordion/Accordion'
 
-function App() {
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end Javascript Framework'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite Javascript Framework among engineers'
+  },
+  {
+    title: 'How d you use React?',
+    content: 'You use React by creating components'
+  }
+]
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      App
+      <Accordion items={items}/>
     </div>
   );
 }
