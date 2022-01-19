84558d022aebe720e669500b06f30c620089326f
AlbertoGonzalezSaenz <alberto.gonzalez@kinandcarta.com>
2022-01-19 14:24:18 -0500

    applied cleanup function in useEffect to limit amount of requests with help of the setTimeOut and clearTimeOut methods

--- src/components/translate/Convert.js
+++ src/components/translate/Convert.js
@@ -4,27 +4,38 @@ import axios from 'axios';
 const Convert = ({ language, text }) => {
 
     const [translated, setTranslated] = useState('')
+    const [debouncedText, setDebouncedtext] = useState(text)
     const API_KEY = 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedtext(text)
+        }, 700);
+
+        return () => {
+            clearTimeout(timerId)
+        }
+    }, [text])
 
     useEffect(() => {
         console.log('new lnaguage or text')
         const doTranslation = async () => {
             const {data: { data: { translations }} } = await axios.post('https://translation.googleapis.com/language/translate/v2',{}, {params: {
-                q: text,
+                q: debouncedText,
                 target: language.value,
                 key: API_KEY
             }})
 
             setTranslated(translations[0].translatedText)
         }
-
+        
         doTranslation()
 
 
-    },[language, text])
+    },[language, debouncedText])
     return (
         <div>
-            {translated}
+            <h1 className='ui header'>{translated}</h1>
         </div>
     );
 }
